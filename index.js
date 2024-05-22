97779ece5c305baa788610f9ca4abefc155e823f
Damon sarceno <Damonsarceno@gmail.com>
2024-05-22 12:59:39 -0400

    done

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9-' ]/g, '');
+  }
+  static titleize(str) {
+    const excludedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = str.toLowerCase().split(' ');
+    for (let i = 0; i < words.length; i++) {
+      if (i === 0 || !excludedWords.includes(words[i])) {
+        words[i] = this.capitalize(words[i]);
+      }
+    }
+    return words.join(' ');
+  }
 }
